33394ed58e5607158a58170d18b4fd5ce333bced
Morgan Hertz <mo@Morgans-MacBook-Pro.local>
2017-11-19 19:35:19 -0800

    debugging

--- newUser.py
+++ newUser.py
@@ -9,27 +9,27 @@ conn = sqlite3.connect("data.sqlite3")
 
 def newUserSetup():
     user_email = None
-    while user_email == None:
+    while user_email is None or len(user_email) < 1:
         user_email = input("Email address(es):    ")
 
-    email_type = input("Email type (SMS/Email):    ")
+    email_type = ""
+    while email_type.lower() != "sms" and email_type.lower() != "email":
+        email_type = input("Email type (SMS/Email):    ")
 
     locations = None
-    while locations is None:
+    while locations is None or len(locations) < 5:
         locations = input("Location(s):    ")
 
-    timeSt = None
-    timeEnd = None
     while True:
         timeSt = input("Start time:    ")
         timeEnd = input("End time (in 24hr format):    ")
-        if timeSt is not None and timeEnd is not None and timeSt < timeEnd:
+        if len(timeSt) > 0 and len(timeEnd) > 0 and timeSt < timeEnd:
             break
-        elif timeSt is None or timeEnd is None:
+        elif len(timeSt) < 1 or len(timeEnd) < 1:
             break
         else:
             print("Start time must be before end time.")
-    if timeSt is None and timeEnd is None:
+    if len(timeSt) < 1 and len(timeEnd) < 1:
         time = None
     else:
         time = timeSt + "," + timeEnd
@@ -56,7 +56,7 @@ def newUserSetup():
 
     cur = conn.cursor()
     cur.execute("SELECT id FROM Users WHERE email_to LIKE :email",{"email": user_email})
-    if cur.fetchone() is None or len(cur.fetchone()) == 0:
+    if cur.fetchone() is None:
         cur.execute("""INSERT INTO Users (email_to, email_type, locations, time, alerts, forecast_days, tz)
                         VALUES (:email,:type,:loc,:time,:alerts,:days,:tz)""",
                     {"email": user_email,"type": email_type, "loc": locations, "time": time, "alerts": alerts,"days": forecast_days, "tz": tz})
@@ -72,13 +72,13 @@ def newUserSetup():
                             locations = :loc, 
                             time = :time, 
                             alerts = :alerts, 
-                            forecast_days = :days
+                            forecast_days = :days,
                             tz = :tz
                             WHERE id = :id""",
                         {"email": user_email,"type": email_type, "loc": locations, "time": time, "alerts": alerts,"days": forecast_days, "tz": tz, "id": user_id})
+            conn.commit()
         else:
             pass
-        conn.commit()
 
     cur.close()
 
